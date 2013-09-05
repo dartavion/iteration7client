aad8b3c4b673a7a625b915c5044750baeae832a9
Fadi Eliya <fadi.eliya@digitalscientists.com>
2013-09-05 12:24:24 -0400

    Fixes vagrant dev environment

--- Gruntfile.js
+++ Gruntfile.js
@@ -72,7 +72,7 @@ module.exports = function (grunt) {
       options: {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost'
+        hostname: '0.0.0.0'
       },
       livereload: {
         options: {
@@ -365,7 +365,7 @@ module.exports = function (grunt) {
       'concurrent:server',
       'autoprefixer',
       'connect:livereload',
-      'open',
+      // 'open',
       'watch'
     ]);
   });
